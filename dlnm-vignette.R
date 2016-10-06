6276b23b4fcc0837c85dffbe2a623f05ea6d90b7
bprest <brian.prest@duke.edu>
2016-10-06 10:11:22 -0400

    Playing with dlnm vignette

--- dlnm-vignette.R
+++ dlnm-vignette.R
@@ -12,7 +12,41 @@ as.data.table(cb1.pm)[is.na(v1.l1), .N]
 summary(cb1.pm)
 
 library(splines)
-model1 = glm(death ~ cb1.pm + cb1.temp + ns(time, 7*14) + dow, family=gaussian(), data=chicagoNMMAPS)
+# model1 = glm(death ~ cb1.pm + cb1.temp + ns(time, 7*14) + dow, family=gaussian(), data=chicagoNMMAPS)
+model1 = glm(death ~ cb1.pm + cb1.temp + ns(time, 7*14) + dow, family=quasipoisson(), data=chicagoNMMAPS)
+summary(model1)
+
+
+pred1.pm <- crosspred(cb1.pm, model1, at=0:20, bylag=0.2, cumul=TRUE)
+# pred1.pm <- crosspred(cb1.pm, model1, at=0:20, bylag=2, cumul=TRUE)
+
+par(mfrow=c(1,2))
+plot(pred1.pm, "slices", var=20, col=3, ylab="RR", ci.arg=list(density=15,lwd=2),
+     main="Association with a 10-unit increase in PM10")
+lines(pred1.pm, "slices", var=10, col=3, ylab="RR", ci.arg=list(density=15,lwd=2),
+     main="Association with a 10-unit increase in PM10", lty=2)
+plot(pred1.pm, "slices", var=20, col=2, cumul=TRUE, ylab="Cumulative RR",
+     main="Cumulative association with a 10-unit increase in PM10")
+lines(pred1.pm, "slices", var=10, col=2, cumul=TRUE, ylab="Cumulative RR",
+     main="Cumulative association with a 10-unit increase in PM10", lty=2)
+
+
+pred1.pm$allRRfit["10"]
+
+
+cbind(pred1.pm$allRRlow, pred1.pm$allRRhigh)["10",]
+
+
+
+### Play around
+
+library(dlnm)
+cb1.pm <- crossbasis(chicagoNMMAPS$pm10, lag=15, argvar=list(fun="lin"),
+                     arglag=list(fun="poly",degree=2))
+
+summary(cb1.pm)
+
+model1 = glm(death ~ cb1.pm, family=gaussian(), data=chicagoNMMAPS)
 summary(model1)
 
 
@@ -24,3 +58,4 @@ plot(pred1.pm, "slices", var=10, col=3, ylab="RR", ci.arg=list(density=15,lwd=2)
      main="Association with a 10-unit increase in PM10")
 plot(pred1.pm, "slices", var=10, col=2, cumul=TRUE, ylab="Cumulative RR",
      main="Cumulative association with a 10-unit increase in PM10")
+
