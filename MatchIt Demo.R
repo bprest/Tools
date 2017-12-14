534f1da7ea6b769d47a2fb7f3a15d71b86f0ec99
bprest <brian.prest@duke.edu>
2017-12-14 09:06:39 -0500

    MatchIt demo

---
+++ MatchIt Demo.R
@@ -0,0 +1,23 @@
+library(MatchIt)
+data(lalonde)
+head(lalonde)
+dim(lalonde)
+
+demo(exact)
+
+
+demo(subclass)
+
+
+demo(nearest)
+
+
+# left off here
+demo(optimal)
+
+
+demo(full)
+
+demo(cem)
+
+# Left off on page 14 of /Dropbox/reading/matchit.pdf
